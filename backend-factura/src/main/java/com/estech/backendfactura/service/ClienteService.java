8222be5e497e416a4a7737ffd3f989b121277c5e
Enrique <enrikus2004@gmail.com>
2024-02-27 21:03:49 +0100

    Listar clientes

--- backend-factura/src/main/java/com/estech/backendfactura/service/ClienteService.java
+++ backend-factura/src/main/java/com/estech/backendfactura/service/ClienteService.java
@@ -1,5 +1,7 @@
 package com.estech.backendfactura.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +29,10 @@ public class ClienteService {
 
         return clienteFacturaDTO;
 
+    }
+
+    public List<Cliente> listarClientes(){
+        return clienteRepository.findAll();
     }
     
 }
